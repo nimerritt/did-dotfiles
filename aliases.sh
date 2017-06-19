20e3db8bf9a83787c3b67450319828150fd1e52b
Nicholas Merritt <nimerritt@gmail.com>
2017-06-19 11:35:03 +0200

    add java-work alias

--- aliases.sh
+++ aliases.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 alias work='docker run -v $(pwd):/workspace -w /workspace  -it $(pinata-ssh-mount) nimerritt/did-neovim /bin/bash'
+alias java-work='docker run -v $(pwd):/workspace -w /workspace  -it $(pinata-ssh-mount) nimerritt/did-java /bin/bash'
